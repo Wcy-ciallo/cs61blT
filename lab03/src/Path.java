4b958dbcb683fa26a9f1fd0edb5500af06f9a6a1
远 <a@MateBook-GT14.local>
2025-08-09 12:51:28 +0800

    finish lab03

--- lab03/src/Path.java
+++ lab03/src/Path.java
@@ -2,5 +2,37 @@
 public class Path {
 
     // TODO
+    private Point curr;
+    private Point next;
 
+    public Path(double x, double y) {
+        this.curr = new Point();
+        this.next = new Point(x, y);
+    }
+
+    public double getCurrX() {
+        return this.curr.getX();
+    }
+    public double getCurrY() {
+        return this.curr.getY();
+    }
+    public double getNextX() {
+        return this.next.getX();
+    }
+    public double getNextY() {
+        return this.next.getY();
+    }
+
+    public Point getCurrentPoint() {
+        return this.curr;
+    }
+
+    public void setCurrentPoint(Point point) {
+        this.curr = point;
+    }
+
+    public void iterate(double dx, double dy) {
+        this.curr = this.next;
+        this.next = new Point(this.getNextX() + dx, this.getNextY() + dy);
+    }
 }
